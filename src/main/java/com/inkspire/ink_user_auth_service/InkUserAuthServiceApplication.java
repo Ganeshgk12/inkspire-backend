b1fe76795b34c28596bdc9a06d5699cd6b25b56c
GaneshKumar <ganeshvkavi@gmail.com>
2025-09-01 09:42:42 +0530

    Initial commit - Auth service setup

---
+++ src/main/java/com/inkspire/ink_user_auth_service/InkUserAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.inkspire.ink_user_auth_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InkUserAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InkUserAuthServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inkspire/ink_user_auth_service/InkUserAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inkspire.ink_user_auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InkUserAuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
